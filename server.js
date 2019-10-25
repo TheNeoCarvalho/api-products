f77b72a4fdd7c610aad74dbc7a11e59f6745afcd
Manoel Carvalho <manoelmecat@gmail.com>
2019-10-25 08:23:29 -0300

    Terminado - GET_ALL, GET_BY_NAME, GET_BY_ID

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const morgan = require("morgan");
+const products = require("./api.json");
+
+const app = express();
+
+app.use(morgan("combined"));
+
+app.get("/", (req, res) => {
+  res.status(200).json(products);
+});
+
+app.get("/products/:id", (req, res) => {
+  const { id } = req.params;
+  const prods = products.products;
+  prods.map(product => {
+    if (product.id == id) {
+      const p = product;
+      return res.status(200).json(p);
+    }
+  });
+  return res.json({ error: "not found" });
+});
+
+app.get("/products", (req, res) => {
+  const { text } = req.query;
+  const prod = [];
+  const prods = products.products;
+  if (text) {
+    prods.map(product => {
+      if (product.name.includes(text)) {
+        prod.push(product);
+      }
+    });
+    return res.status(200).json({ prod });
+  }
+  return res.status(401).json({ erro: "NOT FOUND" });
+});
+
+app.listen(3003);
